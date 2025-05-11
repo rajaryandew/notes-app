12a93568790d3edfcb1cad69995474fb17507c65
RajArya3347Y <raajaaryandewangan@gmail.com>
2025-05-11 20:32:16 +0530

    Revert "Merge pull request #1 from RajArya3347Y/develop"
    This reverts commit aeacc966d40b3452291d99f90ec754d9e6c3421e, reversing
    changes made to 17b24aeb5d14d4070623a76eeac5977b0b4b29f5.

--- src/components/theme-provider.tsx
+++
@@ -1,8 +0,0 @@
-import { ThemeProvider as NextThemeProvider } from "next-themes";
-
-export function ThemeProvider({
-    children,
-    ...props
-}: React.ComponentProps<typeof NextThemeProvider>){
-    return <NextThemeProvider {...props}>{children}</NextThemeProvider>
-}
