be63853129a163bb719f8cdeb505cb274d8ff84a
Raj Aryan <raajaaryandewangan@gmail.com>
2025-05-15 19:06:18 +0530

    Update set-cookie.ts
    Minor changes

--- src/lib/set-cookie.ts
+++ src/lib/set-cookie.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers";
 import { type User } from "./types";
 
 export async function setUsernameCookie(user: User) {
-  const cookieStore = cookies();
+  const cookieStore = await cookies();
   cookieStore.set("username", user.username, {
     path: "/",
     maxAge: 60 * 60 * 24 * 7,
