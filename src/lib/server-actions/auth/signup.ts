85531f790a8ee48a29796c7f1cfa2ad923f8f50d
RajArya3347Y <raajaaryandewangan@gmail.com>
2025-05-25 16:45:01 +0530

    refactor:Just refactor code to fix eslint errors

--- src/lib/server-actions/auth/signup.ts
+++ src/lib/server-actions/auth/signup.ts
@@ -4,7 +4,6 @@ import { addUser } from "../../db/user"
 import { redirect } from "next/navigation"
 import { revalidatePath } from "next/cache"
 import { setUsernameCookie } from "../../set-cookie"
-import { cookies } from "next/headers"
 
 export async function signup(formData:FormData) {
     const user:User = Object.fromEntries(formData) as User
