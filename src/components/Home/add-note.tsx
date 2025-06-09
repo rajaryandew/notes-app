742d94491cac4d699b001a6ca473c35d518458ed
RajArya3347Y <raajaaryandewangan@gmail.com>
2025-06-09 19:42:01 +0530

    refactor: Removed unused code

--- src/components/Home/add-note.tsx
+++ src/components/Home/add-note.tsx
@@ -1,31 +1,6 @@
 "use client";
 
-import {
-    DialogHeader,
-    Dialog,
-    DialogContent,
-    DialogTitle,
-    DialogTrigger,
-    DialogDescription,
-    DialogClose,
-} from "../ui/dialog";
-import {
-    DrawerHeader,
-    Drawer,
-    DrawerContent,
-    DrawerTitle,
-    DrawerTrigger,
-    DrawerDescription,
-    DrawerClose,
-} from "../ui/drawer";
-import { Input } from "../ui/input";
-import { Label } from "../ui/label";
-import { Textarea } from "../ui/textarea";
-import { useMediaQuery } from "react-responsive";
-import { useEffect, useState } from "react";
-import { MotionButton } from "../ui/motion";
-import { useNote } from "@/context/NoteContext";
-import { addNote } from "@/lib/note-client";
+
 import { ButtonVariant } from "@/lib/types";
 import { AddNoteDialog } from "./add-note-dialog";
 import { AddNoteDrawer } from "./add-note-drawer";
