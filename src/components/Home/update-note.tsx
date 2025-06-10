4ce1646439a2f44086546ebbe50112528294aabc
RajArya3347Y <raajaaryandewangan@gmail.com>
2025-06-10 11:39:23 +0530

    refactor(eslint): Removed unused code

--- src/components/Home/update-note.tsx
+++ src/components/Home/update-note.tsx
@@ -1,20 +1,7 @@
 "use client";
-import { Button } from "../ui/button";
-import {
-    Dialog,
-    DialogClose,
-    DialogHeader,
-    DialogTrigger,
-    DialogContent,
-    DialogDescription,
-    DialogTitle,
-} from "../ui/dialog";
-import { Label } from "../ui/label";
-import { Textarea } from "../ui/textarea";
-import { Input } from "../ui/input";
+
 import { Note } from "@/lib/types";
-import { Dispatch, SetStateAction, useState } from "react";
-import { editNote } from "@/lib/note-client";
+import { Dispatch, SetStateAction} from "react";
 import UpdateNoteDialog from "./update-note-dialog";
 import UpdateNoteDrawer from "./update-note-drawer";
 
